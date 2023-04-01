efed329850f2a02eefdc3e72f667d19263c02925
Prooksius <prooksius@gmail.com>
2023-04-01 18:01:56 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+			<section>
+				<div className="container">
+					<h2>Some heading line</h2>
+					<h3>Some subheading</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+						laboriosam iure cumque obcaecati officia! Numquam totam ab ducimus
+						dolor atque fuga quae soluta, ratione beatae perspiciatis vitae sint
+						excepturi sequi.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Reprehenderit sequi harum unde accusantium ad expedita aut
+						recusandae sunt, ratione a libero beatae, est facere sit soluta,
+						vitae eaque nobis delectus.
+					</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+						adipisci eaque similique dolores laborum libero placeat? Aut quis ea
+						accusantium error, eius recusandae vel voluptatem illo nulla omnis.
+						Ad, deserunt dolorem facere odit placeat illo voluptatem accusamus
+						non fuga, minima quod in tempore architecto a corrupti consequuntur
+						id delectus reprehenderit? Iste at, quod necessitatibus soluta
+						corporis laboriosam, ea unde sapiente fuga debitis reprehenderit
+						mollitia repudiandae voluptas incidunt non omnis nihil libero nam,
+						laudantium adipisci perferendis molestias deleniti in iusto.
+						Perferendis ex labore et? Tempore illo explicabo culpa voluptate at
+						suscipit magnam doloribus. Ratione temporibus aliquam, minima natus
+						animi error praesentium?
+					</p>
+				</div>
+			</section>
+		</div>
+	)
 }
 
-export default App;
+export default App
